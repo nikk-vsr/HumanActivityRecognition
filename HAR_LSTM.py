b603f60364e380047332b2363218d3e5d9f05b51
Nikk.Vsr <nikhilvaliveti@gmail.com>
2025-09-07 22:40:54 +0530

    Create HAR_LSTM.py
    Training dataset preparation

---
+++ HAR_LSTM.py
@@ -0,0 +1,117 @@
+!wget https://archive.ics.uci.edu/ml/machine-learning-databases/00240/UCI%20HAR%20Dataset.zip
+
+# !unzip "UCI HAR Dataset.zip"
+
+import numpy as np
+# Useful Constants
+
+# Those are separate normalised input features for the neural network
+INPUT_SIGNAL_TYPES = [
+    "body_acc_x_",
+    "body_acc_y_",
+    "body_acc_z_",
+    "body_gyro_x_",
+    "body_gyro_y_",
+    "body_gyro_z_",
+    "total_acc_x_",
+    "total_acc_y_",
+    "total_acc_z_"
+]
+
+# Output classes to learn how to classify
+LABELS = [
+    "WALKING", 
+    "WALKING_UPSTAIRS", 
+    "WALKING_DOWNSTAIRS", 
+    "SITTING", 
+    "STANDING", 
+    "LAYING"
+]
+
+
+TRAIN = "train/"
+TEST = "test/"
+DATASET_PATH = "UCI HAR Dataset/"
+
+def load_X(X_signals_paths):
+    X_signals = []
+    
+    for signal_type_path in X_signals_paths:
+        file = open(signal_type_path, 'r')
+        # Read dataset from disk, dealing with text files' syntax
+        X_signals.append(
+            [np.array(serie, dtype=np.float32) for serie in [
+                row.replace('  ', ' ').strip().split(' ') for row in file  #splits the numbers between spaces
+            ]]
+        )
+        file.close()
+    
+    return np.transpose(np.array(X_signals), (1, 2, 0)) # prevous 0 1 2 dimension now becomes 1 2 0
+    # desired shape (number of examples, number of time steps, number of input signals).
+
+X_train_signals_paths = [
+    DATASET_PATH + TRAIN + "Inertial Signals/" + signal + "train.txt" for signal in INPUT_SIGNAL_TYPES
+]
+X_test_signals_paths = [
+    DATASET_PATH + TEST + "Inertial Signals/" + signal + "test.txt" for signal in INPUT_SIGNAL_TYPES
+]
+
+X_train = load_X(X_train_signals_paths)
+X_test = load_X(X_test_signals_paths)
+print(X_train_signals_paths)
+
+
+
+def load_y(y_path):
+    file = open(y_path, 'r')
+    # Read dataset from disk, dealing with text file's syntax
+    y_ = np.array(
+        [elem for elem in [
+            row.replace('  ', ' ').strip().split(' ') for row in file
+        ]], 
+        dtype=np.int32
+    )
+    file.close()
+    
+    # Substract 1 to each output class for friendly 0-based indexing 
+    return y_ - 1
+
+y_train_path = DATASET_PATH + TRAIN + "y_train.txt"
+y_test_path = DATASET_PATH + TEST + "y_test.txt"
+
+y_train = load_y(y_train_path)
+y_test = load_y(y_test_path)
+
+
+training_data_count = len(X_train)  # 7352 training series (with 50% overlap between each serie)
+test_data_count = len(X_test)  # 2947 testing series
+n_steps = len(X_train[0])  # 128 timesteps per series
+n_input = len(X_train[0][0])  # 9 input parameters per timestep
+
+
+print(X_train.shape)  #128 timesteps is already given in the data file # 50% overlap is there between sequence to sequence
+print(X_train[0].shape)
+print(X_train[0][0])
+
+
+# LSTM Neural Network's internal structure
+
+n_hidden = 32 # Hidden layer num of features
+n_classes = 6 # Total classes (should go up, or should go down)
+
+
+# Training 
+
+learning_rate = 0.0025
+lambda_loss_amount = 0.0015
+training_iters = training_data_count * 300  # Loop 300 times on the dataset
+batch_size = 1500
+display_iter = 30000  # To show test set accuracy during training
+
+
+# Some debugging info
+
+print("Some useful info to get an insight on dataset's shape and normalisation:")
+print("(X shape, y shape, every X's mean, every X's standard deviation)")
+print(X_test.shape, y_test.shape, np.mean(X_test), np.std(X_test))
+print("The dataset is therefore properly normalised, as expected, but not yet one-hot encoded.")
