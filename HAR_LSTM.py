81229c51eea33f7e59e440b1cba48e7a62678f2d
Nikk.Vsr <nikhilvaliveti@gmail.com>
2025-09-09 23:42:08 +0530

    Update HAR_LSTM.py
    added confusion matrix

--- HAR_LSTM.py
+++ HAR_LSTM.py
@@ -225,3 +225,46 @@ with torch.no_grad():
     final_acc = (final_preds == y_test_t).float().mean().item()
 
 print("FINAL RESULT: Batch Loss = {:.6f}, Accuracy = {:.6f}".format(final_loss.item(), final_acc))
+
+
+# Results
+
+predictions = one_hot_predictions.argmax(1)
+
+print("Testing Accuracy: {}%".format(100*accuracy))
+
+print("")
+print("Precision: {}%".format(100*metrics.precision_score(y_test, predictions, average="weighted")))
+print("Recall: {}%".format(100*metrics.recall_score(y_test, predictions, average="weighted")))
+print("f1_score: {}%".format(100*metrics.f1_score(y_test, predictions, average="weighted")))
+
+print("")
+print("Confusion Matrix:")
+confusion_matrix = metrics.confusion_matrix(y_test, predictions)
+print(confusion_matrix)
+normalised_confusion_matrix = np.array(confusion_matrix, dtype=np.float32)/np.sum(confusion_matrix)*100
+
+print("")
+print("Confusion matrix (normalised to % of total test data):")
+print(normalised_confusion_matrix)
+print("Note: training and testing data is not equally distributed amongst classes, ")
+print("so it is normal that more than a 6th of the data is correctly classifier in the last category.")
+
+# Plot Results:
+width = 12
+height = 12
+plt.figure(figsize=(width, height))
+plt.imshow(
+    normalised_confusion_matrix,
+    interpolation='nearest',
+    cmap=plt.cm.rainbow
+)
+plt.title("Confusion matrix \n(normalised to % of total test data)")
+plt.colorbar()
+tick_marks = np.arange(n_classes)
+plt.xticks(tick_marks, LABELS, rotation=90)
+plt.yticks(tick_marks, LABELS)
+plt.tight_layout()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
